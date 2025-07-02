ddb56fdc013cf35157e8590fabb79d6fa17c7e3b
shion-3 <167150262+shion-3@users.noreply.github.com>
2025-07-02 04:40:07 +0000

    test

--- test .py
+++ test .py
@@ -4,7 +4,7 @@ def example1():
     ####This is a long comment. This should be wrapped to fit within 72 characters.
     some_tuple=(   1,2, 3,'a'  );
     some_variable={'long':'Long code lines should be wrapped within 79 characters.',
-    'other':[math.pi, 100,200,300,9876543210,'This is a long string that goes on'],
+    'other':[math.pi, 100,200,3000,9876543210,'This is a long string that goes on'],
     'more':{'inner':'This whole logical line should be wrapped.',some_tuple:[1,
     20,300,40000,50000000,60000000000000000]}}
     return (some_tuple, some_variable)
